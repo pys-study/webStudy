85a472505fecc8f126319dbd745c4ca025cf032f
asd0236 <tkdgns0236@gmail.com>
2023-09-18 16:29:23 +0900

    Docs: 박상훈 1주차 study-spring 폴더 수정

--- bagsanghun/study-spring/src/main/java/study/studyspring/controller/StudyController.java
+++ bagsanghun/study-spring/src/main/java/study/studyspring/controller/StudyController.java
@@ -1,2 +1,15 @@
-package study.studyspring.controller;public class StudyController {
+package study.studyspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class StudyController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data", "hello!!");
+        return "hello";
+    }
 }
