2d8dc14a0cb47d396d3f32de24cddaed4f0fe547
asd0236 <tkdgns0236@gmail.com>
2023-09-19 16:50:00 +0900

    Docs: 박상훈 1주차 README 파일 갱신

--- bagsanghun/study-spring/src/main/java/study/studyspring/controller/StudyController.java
+++ bagsanghun/study-spring/src/main/java/study/studyspring/controller/StudyController.java
@@ -1,8 +1,12 @@
 package study.studyspring.controller;
 
+import org.apache.tomcat.util.net.TLSClientHelloExtractor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.service.annotation.GetExchange;
 
 @Controller
 public class StudyController {
@@ -11,5 +15,39 @@ public class StudyController {
     public String hello(Model model){
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    // 템플릿 방식
+    @GetMapping("hello-string")
+    @ResponseBody // http에서 <body> 부분에 아래 데이터를 직접 넣겠다는것을 명시
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
+    }
+
+    // api 방식
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
