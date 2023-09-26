af928ec5a9036a04b0e1b48e52b07c8bb1f54b4d
MCshin00 <sinminc7@gmail.com>
2023-09-26 16:30:12 +0900

    Feat : Todo App 로그인 기능 구현

---
+++ smc/fullstack_todoapps/src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import TodoApp from './components/todo/TodoApp';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoApp />
+    </div>
+  );
+}
+
+// function PlayingWithProps(properties) {
+//     console.log(properties.property1)
+//     return (
+//       <div>Props</div>
+//     )
+// }
+
+// function PlayingWithProps({property1, property2}) {
+//     console.log(property1)
+//     console.log(property2)
+    
+//       return (
+//         <div>Props</div>
+//       )
+//   }
+
+export default App;
---
+++ smc/fullstack_todoapps/src/components/todo/TodoApp.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react'
+import './TodoApp.css'
+
+export default function TodoApp() {
+    return (
+        <div className="TodoApp">
+            Todo Management Application
+            <LoginComponent />
+            {/* <WelcomeComponent /> */}
+        </div>
+    )
+}
+
+function LoginComponent() {
+
+    const [username, setUsername] = useState('in28minutes')
+
+    const [password, setPassword] = useState('')
+
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
+
+    const [showErrorMessage, setShowErrorMessage] = useState(false)
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
+    function handleSubmit() {
+        if(username==='in28minutes' && password==='dummy'){
+            console.log('Success')
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+        }
+        else {
+            console.log('Failed')
+            setShowSuccessMessage(false)
+            setShowErrorMessage(true)
+        }
+    }
+
+    return (
+        <div className="Login">
+            {showSuccessMessage && <div className='successMessage'>Authenticated Successfully</div>}
+            {showErrorMessage && <div className='errorMessage'>Authenticated Failed. Please Check your credentials.</div>}
+            <div className="LoginForm">
+                <div>
+                    <label>User Name</label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
+                </div>
+                <div>
+                    <label>Password</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
+                </div>
+                <div>
+                    <button type="button" name="login" onClick={handleSubmit}>login</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function WelcomeComponent() {
+    return (
+        <div className="Welcome">
+            Welcome Component
+        </div>
+    )
+}
