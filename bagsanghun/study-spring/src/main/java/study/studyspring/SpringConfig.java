7dd8d2994597648f1cc10ccd1c88444a47346fe0
asd0236 <tkdgns0236@gmail.com>
2023-09-26 16:24:24 +0900

    Feat: 박상훈 2주차 코드 갱신

---
+++ bagsanghun/study-spring/src/main/java/study/studyspring/SpringConfig.java
@@ -0,0 +1,21 @@
+package study.studyspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import study.studyspring.repository.MemberRepository;
+import study.studyspring.repository.MemoryMemberRepository;
+import study.studyspring.service.MemberService;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean // 직접 스프링 빈에 등록
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
