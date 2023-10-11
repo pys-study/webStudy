e7b90770ac81ba005e04c0a4ffa4f534f3f6b362
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-10-11 23:06:05 +0900

    Feat : 2주차 계산기 파일 분리

--- ydu/2주차/carculator.js
+++ ydu/2주차/carculator.js
@@ -14,3 +14,81 @@
 // 2. 연산자 버튼 클릭
 // 3. = 버튼 클릭
 
+let numOne = '';
+let operator = '';
+let numTwo = '';
+const $operator = document.querySelector('#operator');
+const $result = document.querySelector('#result');
+// 태그 저장시 $표시 붙이기
+
+
+
+const onClickButton = (event) => {
+  if (!operator) {  // 연산자 입력 받지 않았을 경우
+    numOne += event.target.textContent;
+    $result.value += event.target.textContent;
+    return;
+  }
+  // 연산자가 있을 때
+  if (!numTwo) {
+    $result.value = "";
+  }
+  numTwo += event.target.textContent;
+  $result.value += event.target.textContent;
+
+};
+
+document.querySelector('#num-0').addEventListener('click', onClickButton);
+document.querySelector('#num-1').addEventListener('click', onClickButton);
+document.querySelector('#num-2').addEventListener('click', onClickButton);
+document.querySelector('#num-3').addEventListener('click', onClickButton);
+document.querySelector('#num-4').addEventListener('click', onClickButton);
+document.querySelector('#num-5').addEventListener('click', onClickButton);
+document.querySelector('#num-6').addEventListener('click', onClickButton);
+document.querySelector('#num-7').addEventListener('click', onClickButton);
+document.querySelector('#num-8').addEventListener('click', onClickButton);
+document.querySelector('#num-9').addEventListener('click', onClickButton);
+
+
+const onClickOperator = (op) => () => {
+  if (numOne) {
+    operator = op;
+    $operator.value = op;
+  } else {
+    alert('숫자를 먼저 입력하세요.');
+  }
+}
+
+document.querySelector('#plus').addEventListener('click', onClickOperator('+'));
+document.querySelector('#minus').addEventListener('click', onClickOperator('-'));
+document.querySelector('#divide').addEventListener('click', onClickOperator('/'));
+document.querySelector('#multiply').addEventListener('click', onClickOperator('*'));
+document.querySelector('#calculate').addEventListener('click', () => {
+  if (numTwo) // 두 번째 숫자가 입력 됐을 경우
+  {
+    switch (operator) {
+      case '+':
+        $result.value = eval(numOne + operator + numTwo);
+        break;
+      case '-':
+        $result.value = numOne - numTwo;
+        break;
+      case '*':
+        $result.value = numOne * numTwo;
+        break;
+      case '/':
+        $result.value = numOne / numTwo;
+      default:
+        break;
+    }
+  } else {
+    alert("숫자를 먼저 입력하세요 ");
+  }
+});
+document.querySelector('#clear').addEventListener('click', () => {
+  numOne = '';
+  numTwo = '';
+  $result.value = '';
+  operator = '';
+  $operator.value = '';
+});
