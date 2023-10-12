9da6ffa30c5b25e8eb51766584800c3aa7257bf5
MCshin00 <sinminc7@gmail.com>
2023-10-12 23:51:20 +0900

    Feat : Todo App에 할 일 목록 컴포넌트 추가

--- smc/fullstack_todoapps/src/components/todo/TodoApp.jsx
+++ smc/fullstack_todoapps/src/components/todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter, Route, Routes, useNavigate, useParams} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useNavigate, useParams, Link} from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
@@ -10,6 +10,8 @@ export default function TodoApp() {
                 <Route path ='/' element={<LoginComponent />}></Route>
                     <Route path ='/login' element={<LoginComponent />}></Route>
                     <Route path ='/welcome/:username' element={<WelcomeComponent />}></Route>
+                    <Route path ='/todos' element={<ListTodosComponent />}></Route>
+             
                     <Route path ='*' element={<ErrorComponent />}></Route>
                 </Routes>
             </BrowserRouter>
@@ -81,7 +83,8 @@ function WelcomeComponent() {
         <div className="WelcomeComponent">
             <h1>Welcome {username}</h1>
             <div>
-            Welcome Component
+                Manage Your todos - <Link to="/todos">Go here</Link>
+
             </div>
         </div>
         
@@ -94,6 +97,51 @@ function ErrorComponent() {
             <h1>We are working really hard!</h1>
             <div>
                 404 NOT FOUND
+            </div>
+        </div>
+    )
+}
+
+function ListTodosComponent() {
+    
+    const today = new Date()
+    const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDate())
+
+    const todos = [
+                    {id: 1, description: 'Learn AWS', done: false, targetDate: targetDate},
+                    {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate},
+                    {id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate}
+                ]
+
+    return (
+        <div className="ListTodosComponent">
+            <h1>Things you Want To Do!</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <td>id</td>
+                            <td>description</td>
+                            <td>is Done?</td>
+                            <td>Target Date</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            todos.map(
+                                todo => (
+                                    <tr key={todo.id}>
+                                        <td>{todo.id}</td>
+                                        <td>{todo.description}</td>
+                                        <td>{todo.done.toString()}</td>
+                                        <td>{todo.targetDate.toDateString()}</td>
+                                    </tr>
+                                )
+                            )
+                        }
+                    </tbody>
+
+                </table>
             </div>
         </div>
     )
