7c7b41916571e8cae555c59febf2d900fe9f9872
도운 <dbsehdns12@gmail.com>
2023-11-16 16:42:46 +0900

    Feat : 홈페이지 초안 작성

---
+++ teamHompage/teamHp.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+  // 팝업 열기
+  const popupButton = document.getElementById('popupButton');
+  const popup = document.getElementById('popup');
+  const popupCloseButton = document.getElementById('popupCloseButton');
+
+  popupButton.addEventListener('click', function () {
+    popup.style.display = 'block';
+  });
+
+  popupCloseButton.addEventListener('click', function () {
+    popup.style.display = 'none';
+  });
+
+  // 입력 기능
+  const inputField = document.getElementById('inputField');
+  inputField.addEventListener('keyup', function () {
+    console.log('입력값:', inputField.value);
+  });
+});
