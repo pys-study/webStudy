9355402b788927c8280f6f75ff29d72cf24c0f94
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-11-17 23:19:48 +0900

    Refactor : 진행바 변수명 변경 & 헤더 높이 수정

--- teamHomepage/teamHp.js
+++ teamHomepage/teamHp.js
@@ -10,13 +10,13 @@ let scrollTop = 0;
 let bar;
 
 window.onload = function () {
-  bar = document.getElementsByClassName("bar-ing")[0];
+  bar = document.getElementsByClassName("progressBar-ing")[0];
 };
 window.addEventListener('scroll', function () {
   const scrollTop = document.documentElement.scrollTop;
   const documentHeight = document.documentElement.scrollHeight;
   const windowHeight = window.innerHeight;
   const scrollPercent = (scrollTop / (documentHeight - windowHeight)) * 100;
-  const scrollBar = document.querySelector('.scroll-ing');
+  const scrollBar = document.querySelector('.progressBar-ing');
   scrollBar.style.width = scrollPercent + '%';
 });
