14a27e122e42c5a1c68a53e3a33206baf202fbf3
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-11-17 22:57:41 +0900

    Refactor : 불필요한 코드 삭제

--- teamHomepage/teamHp.js
+++ teamHomepage/teamHp.js
@@ -1,23 +1,3 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // 팝업 열기
-  const popupButton = document.getElementById('popupButton');
-  const popup = document.getElementById('popup');
-  const popupCloseButton = document.getElementById('popupCloseButton');
-
-  popupButton.addEventListener('click', function () {
-    popup.style.display = 'block';
-  });
-
-  popupCloseButton.addEventListener('click', function () {
-    popup.style.display = 'none';
-  });
-
-  // 입력 기능
-  const inputField = document.getElementById('inputField');
-  inputField.addEventListener('keyup', function () {
-    console.log('입력값:', inputField.value);
-  });
-});
 
 // top 버튼
 let moveToTop = function () {
