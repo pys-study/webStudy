6eee08520a3eb1a6bbbfac9546f8a33416455959
Do - un Yun <106233376+yundoun@users.noreply.github.com>
2023-11-17 22:45:03 +0900

    Feat : 상단 스크롤바 추가

--- teamHomepage/teamHp.js
+++ teamHomepage/teamHp.js
@@ -24,3 +24,19 @@ let moveToTop = function () {
   document.body.scrollIntoView({ behavior: "smooth" });
 };
 
+
+// 상단 진행바
+let scrollTop = 0;
+let bar;
+
+window.onload = function () {
+  bar = document.getElementsByClassName("bar-ing")[0];
+};
+window.addEventListener('scroll', function () {
+  const scrollTop = document.documentElement.scrollTop;
+  const documentHeight = document.documentElement.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrollPercent = (scrollTop / (documentHeight - windowHeight)) * 100;
+  const scrollBar = document.querySelector('.scroll-ing');
+  scrollBar.style.width = scrollPercent + '%';
+});
